6f32394075bf0df4a1ea3b8f0c5e08f8fc4d23a2
Fabricio <fabriciosan47@gmail.com>
2024-11-04 08:05:03 -0400

    Adiciona Listagem de Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_01_140738) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_01_164934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
